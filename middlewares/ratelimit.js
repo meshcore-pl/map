8a0a047ac745a630b70fd965fc0ec589a986031b
Sefinek <contact@sefinek.net>
2026-07-20 02:04:57 +0200

    HttpError

--- middlewares/ratelimit.js
+++ middlewares/ratelimit.js
@@ -1,12 +1,12 @@
 const rateLimit = require('express-rate-limit');
-const ApiError = require('../utils/httpError.js');
+const HttpError = require('../utils/httpError.js');
 
 const globalLimiter = rateLimit({
 	windowMs: 40 * 1000,
 	limit: 52,
 	standardHeaders: 'draft-7',
 	legacyHeaders: false,
-	handler: (req, res) => ApiError(res, 429),
+	handler: (req, res) => HttpError(res, 429),
 });
 
 module.exports = globalLimiter;
