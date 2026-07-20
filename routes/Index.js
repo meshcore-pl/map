8f7201f99ea8c6018ce46b0952535ecf17901050
Sefinek <contact@sefinek.net>
2026-07-20 04:40:16 +0200

    .ejs

--- routes/Index.js
+++ routes/Index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => res.render('index'));
-router.get('/test', (req, res) => res.render('test'));
+router.get('/', (req, res) => res.render('index.ejs'));
+router.get('/test', (req, res) => res.render('test.ejs'));
 
 module.exports = router;
